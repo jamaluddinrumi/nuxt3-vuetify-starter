13d3a08f03d806572ae92e4359d839fb146d7ba7
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-01-02 03:04:36 +0700

    add essential pages

---
+++ pages/progress-orders.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue?: boolean;
+}>();
+</script>
+
+<template>
+  <div class="d-flex align-center h-screen justify-center">
+    <slot />
+    Progress Orders
+  </div>
+</template>
